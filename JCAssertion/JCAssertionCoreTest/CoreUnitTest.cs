7d5797ae330b337c88d49c5f2df609a1febd59c4
Jean-Claude Parent <noursicain@gmail.com>
2016-02-12 22:22:28 -0500

    Création de la solution

---
+++ JCAssertion/JCAssertionCoreTest/CoreUnitTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JCAssertionCore;
+
+
+
+namespace JCAssertionCoreTest
+{
+    [TestClass]
+    public class CoreUnitTest
+    {
+        [TestMethod]
+        public void CoreLoadOK()
+        {
+            Core monJCACore = new Core();
+            Assert.AreEqual(7, monJCACore ,"Le fichier chargé devrait contenir 7 cas de test, réel = " + monJCACore.NombreCas.ToString()  );
+
+
+
+            
+        }
+    }
+}
---
+++ JCAssertion/JCAssertionnCore/Core.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JCAssertionCore
+{
+    public class Core
+    {
+        public String FichierDeCas;
+        public string FichierJournal;
+        public Boolean Journaliser = true;
+        public int NombreCas = 0;
+        public int CasCourant = 0;
+
+        public void Load(String NomFichierLoad)
+            {
+            }
+
+    }
+}

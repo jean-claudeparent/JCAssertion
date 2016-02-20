20f97283f18ec38c8abe12d9cb64df9966086a5f
Jean-Claude Parent <noursicain@gmail.com>
2016-02-20 10:52:22 -0500

    Finir les cas de test de SubstituerVariable, tout fail

--- JCAssertion/JCAssertionCoreTest/JCAVariableUnitTest1.cs
+++ JCAssertion/JCAssertionCoreTest/JCAVariableUnitTest1.cs
@@ -14,9 +14,16 @@ namespace JCAssertionCoreTest
             monJCACore.MAJVariable("Test2","Ceci estklavaleursubstituée de test 2");
             monJCACore.MAJVariable("Test1", "Ceci estklavaleursubstituée de test 1");
             monJCACore.MAJVariable("Test", "Ceci estklavaleursubstituée");
+            
             Assert.AreEqual("Aucune variable",JCAVariable.SubstituerVariables("Aucune variable",monJCACore.GetDictionnaireVariable()));
-            //Assert.AreEqual("Début:Ceci estklavaleursubstituée:Fin", JCAVariable.SubstituerVariables("", monJCACore.GetDictionnaireVariable));
+            Assert.AreEqual("Début:Ceci estklavaleursubstituée:Fin", JCAVariable.SubstituerVariables("Début:{{Test}}:Fin", monJCACore.GetDictionnaireVariable()));
+            Assert.AreEqual("Ceci estklavaleursubstituée de test 2Début:Ceci estklavaleursubstituée:FinCeci estklavaleursubstituéeCeci estklavaleursubstituée de test 1", JCAVariable.SubstituerVariables("{{Test2}}Début:{{Test}}:Fin{{Test}}{{Test1}}", monJCACore.GetDictionnaireVariable()));
 
+            try  {
+                Assert.AreEqual("Exception attendue", JCAVariable.SubstituerVariables("Début:{{TestVarexistepas}}:Fin", monJCACore.GetDictionnaireVariable()));
+            } catch (JCAssertionException excep) {
+                Assert.IsTrue(excep.Message.Contains("pas fournie"),"Il devrait y avoir un exception si la variable n'est pas fournie." );
+            }
 
         }
     }

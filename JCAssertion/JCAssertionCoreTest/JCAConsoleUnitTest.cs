9e94d0ed0ca603e0c45cb0c611f10b1d6198c381
Jean-Claude Parent <noursicain@gmail.com>
2016-03-02 16:18:07 -0500

    Les test des utilitaires console passent

--- JCAssertion/JCAssertionCoreTest/JCAConsoleUnitTest.cs
+++ JCAssertion/JCAssertionCoreTest/JCAConsoleUnitTest.cs
@@ -23,16 +23,54 @@ namespace JCAssertionCoreTest
             mesArgs[1] = "/on";
             mesArgs[2] = "/v:Cle=valeur";
             mesArgs[3] = "/fic2:c:app avec espace.exe /p";
+            mesArgs[4] = "Test55";
+            mesArgs[5] = "Test43:Valeur43";
+           
             mesVariables = maConsole.Arguments(mesArgs);
             Assert.AreEqual("ON", mesVariables.GetValeurVariable("ON"));
             Assert.AreEqual("c:app.exe", mesVariables.GetValeurVariable("FICHIER"));
             Assert.AreEqual("Cle=valeur", mesVariables.GetValeurVariable("V"));
             Assert.AreEqual("c:app avec espace.exe /p", mesVariables.GetValeurVariable("FIC2"));
+            Assert.AreEqual("Valeur43", mesVariables.GetValeurVariable("TEST43"));
+            Assert.AreEqual("Test55", mesVariables.GetValeurVariable("Test55"));
+            
             Assert.IsNull(mesVariables.GetValeurVariable("FIxxxC2"));
  
 
 
 
         }
+
+        [TestMethod]
+        public void ExtraireParamTest()
+        {
+            JCAssertionCore.JCAConsole maConsole = new JCAssertionCore.JCAConsole();
+            String maCle;
+            String maValeur;
+
+            Assert.IsTrue(maConsole.ExtraireParam ("/duplex", out maCle , out maValeur ));
+            Assert.AreEqual("DUPLEX", maCle );
+            Assert.AreEqual("DUPLEX", maValeur);
+
+            Assert.IsTrue(maConsole.ExtraireParam("/duplex:", out maCle, out maValeur));
+            Assert.AreEqual("DUPLEX", maCle);
+            Assert.AreEqual("", maValeur);
+
+            Assert.IsFalse (maConsole.ExtraireParam("", out maCle, out maValeur));
+            Assert.AreEqual("", maCle);
+            Assert.AreEqual("", maValeur);
+
+            Assert.IsTrue(maConsole.ExtraireParam("/duplex:c:/test:a", out maCle, out maValeur));
+            Assert.AreEqual("DUPLEX", maCle);
+            Assert.AreEqual("c:/test:a", maValeur);
+
+            Assert.IsTrue(maConsole.ExtraireParam("TEST55", out maCle, out maValeur));
+            Assert.AreEqual("TEST55", maCle);
+            Assert.AreEqual("TEST55", maValeur);
+
+
+
+        }
+        
     }
 }
--- JCAssertion/JCAssertionnCore/JCAConsole.cs
+++ JCAssertion/JCAssertionnCore/JCAConsole.cs
@@ -12,7 +12,58 @@ namespace JCAssertionCore
     {
         public JCAVariable  Arguments(string[] args)
         {
-            return new JCAVariable();
+            String Cle;
+            String Valeur;
+            JCAVariable Resultat = new JCAVariable();
+
+            foreach  (String  Param in args  )
+                {
+                    if ((Param != null ) && (Param != "")  )
+                        {
+                            if(ExtraireParam(Param ,out Cle, out Valeur ))
+                                Resultat.MAJVariable (Cle,Valeur );
+                        }
+                }
+            return Resultat;
         }
+
+        public bool   ExtraireParam(string monParam, out String Cle, out String Valeur)
+        {
+            Cle = "";
+            Valeur = "";
+            if(monParam == null) return false ;
+            if (monParam == "") return false;
+
+            if ((monParam.Substring(0,1) == "/" ) && (! monParam.Contains(":") ))
+                // Param de forme /quelquechose mais sans le séparateur de valeur (:),alors normaliser
+                {
+                    monParam = monParam + ":" + monParam.Substring(1,monParam.Length - 1).ToUpper() ;
+                }
+            
+            // le / n'est plus utilel'enlever
+            if (monParam.Substring(0,1) == "/" )
+                {
+                    monParam = monParam.Substring (1, monParam.Length -1);
+                }
+            // extraire vraiment
+            if(monParam.Contains (":"))
+                {
+                    Cle = monParam.Substring(0, monParam.IndexOf (":")).ToUpper ();
+                    Valeur = monParam.Substring(monParam.IndexOf(":") + 1, monParam.Length - Cle.Length  - 1); 
+                } else
+                {
+                    Cle = monParam;
+                    Valeur = monParam;
+                };
+
+
+            if (Cle == "")
+                return false;
+            else
+                return true;
+        }
+        
+
+
     }
 }

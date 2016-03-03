22bc1961e7914c9adb261e3a4a60651aefced75c
Jean-Claude Parent <noursicain@gmail.com>
2016-03-03 16:23:43 -0500

    Les tests d'arguments en erreur passent

--- JCAssertion/JCAExport/JCAExporteCore.cs
+++ JCAssertion/JCAExport/JCAExporteCore.cs
@@ -13,19 +13,36 @@ namespace JCAExporte
         public int ExecuteExporte(string[] args, out String Message)
         {
             JCAVariable mesArgs = new JCAssertionCore.JCAVariable();
+            JCAVariable mesVariables = new JCAssertionCore.JCAVariable();
             JCAConsole maConsole = new JCAssertionCore.JCAConsole ();
+            String Usage = "usage :" + Environment.NewLine + Environment.NewLine + "JCAExporte /F:fichier /V1:cle=valeur /v2=cle2=valeur2 ...";
 
             Message = "";
 
             if (args.Count() < 2)
             {
-                Message = "Pas assez d'argument, usage :" + Environment.NewLine + "JCAExporte /F:fichier /V:cle=valeur";
+                Message = "Pas assez d'arguments. " + Environment.NewLine + Usage ;
                 return 99;
             }
 
             mesArgs = maConsole.Arguments(args);
 
+            if ((mesArgs.GetValeurVariable("F") == null) || (mesArgs.GetValeurVariable("F") == ""))
+                {
+                    Message = "L'argument /F (Chemin du fichier de sortie nMa oas été spécfié.)" + Environment.NewLine + Usage;
+                    return 99;
+                }
 
+            // Traitemen OK
+            String NomFichier = mesArgs.GetValeurVariable("F");
+            if (System.IO.File.Exists (NomFichier)) mesVariables.LireFichier(NomFichier );
+            // Ajouter ou maj les variables
+
+            //Sauvegarder
+            mesVariables.EcrireFichier(NomFichier);
+
+            Message = "Fichier : " + NomFichier + " mis à jour.";
+            
             return 0;
         }
 
--- JCAssertion/JCAssertionTest/UnitTest1.cs
+++ JCAssertion/JCAssertionTest/UnitTest1.cs
@@ -18,19 +18,37 @@ namespace JCAssertionTest
             Assert.AreEqual(99, monExporte.ExecuteExporte(mesArgs , out Message ));
             Assert.IsTrue(Message.ToUpper().Contains("USAGE") ,"Le message devrait donner l'usage du programme : " + Message );
 
-            // oas assez d'argument
+            // pas assez d'argument Cas 2
             Message = null;
             String[] mesArgs1 = new String[1];
-            mesArgs1[1] = "/v1:Test=ok";
-            Assert.AreEqual(99, monExporte.ExecuteExporte(mesArgs1, out Message));
-            Assert.IsTrue(Message.ToUpper().Contains("USAGE"), "Le message devrait donner l'usage du programme : " + Message);
+            mesArgs1[0] = "/v1:Test=ok";
+            try {
+              Assert.AreEqual(99, monExporte.ExecuteExporte(mesArgs1, out Message));
+            } catch (Exception excep)
+            {
+                Assert.Fail("Le cas 2 de ExporttePasOKTest a l'exception : " + excep.Message  );
+            }
+                Assert.IsTrue(Message.ToUpper().Contains("USAGE"), "Le message devrait donner l'usage du programme : " + Message);
 
             
 
             // assez d'argument mais pas d'argument fichier
-
-
-
-        }
+                Message = null;
+                String[] mesArgs2 = new String[3];
+                mesArgs2[1] = "/v1:Test=ok";
+                mesArgs2[2] = "/v2:Test2=ok";
+                Assert.AreEqual(99, monExporte.ExecuteExporte(mesArgs2, out Message));
+            
+            
+            // Nom de fichier invalide
+                mesArgs2[0] = "/f:z:::;";
+                try
+                {
+                    Assert.AreEqual(99, monExporte.ExecuteExporte(mesArgs2, out Message));
+                } catch (Exception excep)
+            {
+                Assert.IsTrue(excep.Message.Contains ("hemin"));
+            }
+          }
     }
 }

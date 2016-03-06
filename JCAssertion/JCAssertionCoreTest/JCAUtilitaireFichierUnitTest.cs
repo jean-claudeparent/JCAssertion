d261c990d95835b66b961a30ea54906e0a9b494e
Jean-Claude Parent <noursicain@gmail.com>
2016-03-05 23:24:02 -0500

    SubstituerVariableFichier passe les tests

---
+++ JCAssertion/JCAssertionCoreTest/JCAUtilitaireFichierUnitTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JCAssertionCore;
+using System.IO;
+
+
+namespace JCAssertionCoreTest
+{
+    [TestClass]
+    public class JCAUtilitaireFichierUnitTest
+    {
+        [TestMethod]
+        public void SubstituerVariableFichierTest()
+        {
+            JCAUtilitaireFichier monUFichier = new JCAUtilitaireFichier();
+
+            // quelques erreurs
+            try {
+                monUFichier.SubstituerVariableFichier(null, "","");
+                } catch (Exception excep)
+            {
+                Assert.IsNotNull(excep );
+            }
+
+            // cas qui marche
+            String monPath = JCACore.RepertoireAssembly() + "\\Ressources\\";
+            JCAVariable mesVariables = new JCAVariable();
+            mesVariables.MAJVariable("Logon","noursicain");
+            mesVariables.MAJVariable("SpoolFile", "z:allo");
+            mesVariables.EcrireFichier (monPath + "SQLOK.xml" );
+
+            if (System.IO.File.Exists(monPath + "SQLOK.SQL"))
+                System.IO.File.Delete(monPath + "SQLOK.SQL");
+            Assert.IsFalse(System.IO.File.Exists(monPath + "SQLOK.SQL"));
+            monUFichier.SubstituerVariableFichier(monPath + "SQLOK.txt", monPath + "SQLOK.SQL", monPath + "SQLOK.xml");
+
+
+
+
+
+
+        }
+    }
+}
---
+++ JCAssertion/JCAssertionnCore/JCAUtilitaireFichier.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using JCAssertionCore;
+
+namespace JCAssertionCore
+{
+    public class JCAUtilitaireFichier
+    {
+        public void SubstituerVariableFichier(String FichierModele, String FichierResultat, String FichierDeVariables)
+        {
+            JCAVariable mesVariables = new JCAVariable();
+            String monContenu;
+
+
+            mesVariables.LireFichier(FichierDeVariables);
+            monContenu = System.IO.File.ReadAllText(FichierModele );
+            monContenu = JCAVariable.SubstituerVariables(monContenu, mesVariables.Variables );
+            System.IO.File.WriteAllText(FichierResultat, monContenu);
+        }
+    }
+}

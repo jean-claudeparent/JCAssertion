10812a9618c0be3674084590f09526a3cfbaaf97
Jean-Claude Parent <noursicain@gmail.com>
2016-03-06 00:08:37 -0500

    Design de base de l'interface utilisateur

--- JCAssertion/JCAssertion/Form1.cs
+++ JCAssertion/JCAssertion/Form1.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace JCAssertion
 {
-    public partial class Form1 : Form
+    public partial class JCAssertion : Form
     {
-        public Form1()
+        public JCAssertion()
         {
             InitializeComponent();
+        }
+
+        private void JCAssertion_Load(object sender, EventArgs e)
+        {
+            // c.est ici que ca se âsse
         }
     }
 }
--- JCAssertion/JCAssertion/Program.cs
+++ JCAssertion/JCAssertion/Program.cs
@@ -16,7 +16,7 @@ namespace JCAssertion
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new JCAssertion());
         }
     }
 }

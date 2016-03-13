daf0dcb0e1e8755fd3f1a451223cbb8f9c650a2f
Jean-Claude Parent <noursicain@gmail.com>
2016-03-13 10:29:54 -0400

    Refactoring pour éliminer du code en double

--- JCAssertion/JCAssertionnCore/JCAPontXML.cs
+++ JCAssertion/JCAssertionnCore/JCAPontXML.cs
@@ -12,13 +12,25 @@ namespace JCAssertionCore
 {
     public   class JCAPontXML
     {
+        public static void ValideBalise(XmlNode monXMLNode, String maBalise)
+            {
+                if (monXMLNode[maBalise] == null)
+                    throw new JCAssertionException("Le XML ne contient pas la balise " + 
+                        maBalise  + "." + monXMLNode.InnerXml);
+                if (monXMLNode[maBalise].InnerText == null)
+                    throw new JCAssertionException("La balise " +
+                        maBalise + " est vide." + monXMLNode.InnerXml);
+                if (monXMLNode[maBalise].InnerText == "")
+                    throw new JCAssertionException("La balise " +
+                        maBalise + " est vide." + monXMLNode.InnerXml);
+            }
+
+
         public static   bool  JCAFichierExiste(XmlNode monXMLNode, ref string   Message,ref  Dictionary<String, String > Variables)
             {
                 Message = Message + Environment.NewLine  + "Assertion FichierExiste\n";
                 if (monXMLNode == null) throw new JCAssertionException("Le XML est vide.");
-                if (monXMLNode["Fichier"] == null) throw new JCAssertionException("Le XML ne contient pas la balise Fichier." + monXMLNode .InnerXml);
-                if (monXMLNode["Fichier"].InnerText == null) throw new JCAssertionException("La balise Fichier est vide." + monXMLNode.InnerXml);
-                if (monXMLNode["Fichier"].InnerText == "") throw new JCAssertionException("La balise Fichier est vide." + monXMLNode.InnerXml);
+                ValideBalise(monXMLNode, "Fichier");
                 string NomFichier = monXMLNode["Fichier"].InnerText;
                 NomFichier = JCAVariable.SubstituerVariables(NomFichier, Variables);
                 Message = Message + Environment.NewLine  +  "Fichier:" + NomFichier  + "\n";
@@ -34,15 +46,9 @@ namespace JCAssertionCore
             if (monXMLNode == null) 
                 throw new JCAssertionException("Le XML est vide.");
             
-            // Valider le fichoer de modèle
-            if (monXMLNode["FichierModele"] == null) 
-                throw new JCAssertionException("Le XML ne contient pas la balise FichierModele." + monXMLNode.InnerXml);
-            if (monXMLNode["FichierModele"].InnerText == null) 
-                throw new JCAssertionException("La balise FichierModele est vide." + monXMLNode.InnerXml);
-            if (monXMLNode["FichierModele"].InnerText == "")
-                throw new JCAssertionException("La balise FichierModele est vide." + monXMLNode.InnerXml);
-            
-            
+            // Valider le fichier de modèle
+
+            ValideBalise(monXMLNode, "FichierModele");
             string NomFichierModele = monXMLNode["FichierModele"].InnerText;
             NomFichierModele = JCAVariable.SubstituerVariables(NomFichierModele, Variables);
             Message = Message + Environment.NewLine + "Fichier de modèle:" + NomFichierModele + Environment.NewLine  ;
@@ -50,28 +56,16 @@ namespace JCAssertionCore
                 throw new JCAssertionException("Le fichier modèle n'existe pas." + monXMLNode.InnerXml);
             
             // Valider le fichier de sortie
-            if (monXMLNode["FichierSortie"] == null)
-                throw new JCAssertionException("Le XML ne contient pas la balise FichierSortie." + monXMLNode.InnerXml);
+            ValideBalise(monXMLNode, "FichierSortie");
             
-            if (monXMLNode["FichierSortie"].InnerText == null)
-                throw new JCAssertionException("La balise FichierSortie est vide." + monXMLNode.InnerXml);
-            
-            if (monXMLNode["FichierSortie"].InnerText == "")
-                throw new JCAssertionException("La balise FichierSortie est vide." + monXMLNode.InnerXml);
             
             string NomFichierSortie = monXMLNode["FichierSortie"].InnerText;
             NomFichierSortie = JCAVariable.SubstituerVariables(NomFichierSortie, Variables);
             Message = Message + Environment.NewLine + "Fichier de sortie:" + NomFichierSortie + Environment.NewLine;
             
             //Valider le fichier de variable
-            // FichierVariables
-
-            if (monXMLNode["FichierVariables"] == null)
-                throw new JCAssertionException("Le XML ne contient pas la balise FichierVariables." + monXMLNode.InnerXml);
-            if (monXMLNode["FichierVariables"].InnerText == null)
-                throw new JCAssertionException("La balise FichierVariables est vide." + monXMLNode.InnerXml);
-            if (monXMLNode["FichierVariables"].InnerText == "")
-                throw new JCAssertionException("La balise FichierVariables est vide." + monXMLNode.InnerXml);
+            
+            ValideBalise(monXMLNode, "FichierVariables");
             string NomFichierVariables = monXMLNode["FichierVariables"].InnerText;
             NomFichierVariables = JCAVariable.SubstituerVariables(NomFichierVariables, Variables);
             Message = Message + Environment.NewLine + "Fichier de variables :" + NomFichierVariables + Environment.NewLine;

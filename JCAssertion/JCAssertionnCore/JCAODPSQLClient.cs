581408d3740eb8d5ca04f90249e769ba18ca3f44
Jean-Claude Parent <noursicain@gmail.com>
2016-08-17 22:23:23 -0400

    Exploration du design de la clsse JCAODPSQLClient

--- JCAssertion/JCAssertionnCore/JCAODPSQLClient.cs
+++ JCAssertion/JCAssertionnCore/JCAODPSQLClient.cs
@@ -3,10 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using JCASQLODPCore;
 
 namespace JCAssertionCore
 {
-    class JCAODPSQLClient
+    public class JCAODPSQLClient
     {
+        private JCASQLODPClient monSQLClient = new JCASQLODPClient();
+        public enum Action {Aucune, Ouvrir, Fermer };
+
+        public void InitConnection(String User,
+            String Password,
+            String Serveur = null,
+            Action monAction = Action.Aucune  )
+        {
+        }
+
+
+        public Boolean SQLAssert(String SQL,
+            String ResultatAttendu)
+        {
+            Boolean Resultat = false;
+            Boolean Fermer = (!monSQLClient.SiConnectionOuverte());
+            if (!monSQLClient.SiConnectionOuverte())
+                monSQLClient.OuvrirConnection();
+            Resultat = monSQLClient.AssertSQL(SQL, ResultatAttendu); 
+            if (Fermer)
+                monSQLClient.FermerConnection();
+
+            return Resultat; 
+        }
+
+        public Boolean SQLAssert(String SQL,
+            Double  ResultatAttendu)
+        {
+            return false;
+        }
+
+
+
     }
 }

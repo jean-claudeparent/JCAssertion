f6f03ed2815b238c4a8a508e56d3355d9f4446b8
Jean-Claude Parent <noursicain@gmail.com>
2016-10-02 13:37:34 -0400

    Squelette du premier unit test

---
+++ JCAssertion/JCASQLODPCoreTest/SQLExecuteUT1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JCASQLODPCore;
+
+
+namespace JCASQLODPCoreTest
+{
+    [TestClass]
+    public class SQLExecuteUT1
+    {
+        JCASQLODPClient monSQLClient = new JCASQLODPClient();
+
+
+
+        [TestInitialize]
+        public void InitTest()
+        {
+            monSQLClient.User = "JCA";
+            monSQLClient.Password = "JCA";
+            monSQLClient.OuvrirConnection();
+            monSQLClient.ActiverResume = true;
+
+        }
+
+        [TestMethod]
+        public void SQLExecuteOK()
+        {
+            Assert.Fail ("Pas encore implémenté.");
+        }
+    }
+}

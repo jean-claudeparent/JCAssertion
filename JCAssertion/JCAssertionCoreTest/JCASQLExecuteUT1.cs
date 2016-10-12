8f80e5cfd5382ca84c66d4e017fe37ce68ec11dd
Jean-Claude Parent <noursicain@gmail.com>
2016-10-12 19:33:51 -0400

    Commit avant exploration
    de définir une classe régulière dans un projet de test

--- JCAssertion/JCAssertionCoreTest/JCASQLExecuteUT1.cs
+++ JCAssertion/JCAssertionCoreTest/JCASQLExecuteUT1.cs
@@ -7,8 +7,23 @@ namespace JCAssertionCoreTest
     [TestClass]
     public class JCASQLExecuteUT1
     {
+        JCACore monCore = new JCACore();
+            
+        [TestInitialize]
+        public void InitTest()
+        {
+            // remplir les variables
+            monCore.Variables.MAJVariable("NomTable","JCATest");
+            monCore.Variables.MAJVariable("Egal", "­=");
+            monCore.Variables.MAJVariable("Colonne", "INFO");
+            monCore.Variables.MAJVariable("cleCas", "UT1SEXML1");
+             
+        }
+        /// <summary>
+        /// SQLExecutexml Test ;e SQ:Execute avec l'interface XML
+        /// </summary>
         [TestMethod]
-        public void TestMethod1()
+        public void SQLExecutexml()
         {
             Assert.Fail("Pas encore implémentée");
         }

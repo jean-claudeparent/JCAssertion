8f96105559a222aadd91cd87e2c8fa6db2ace464
Jean-Claude Parent <noursicain@gmail.com>
2016-12-29 16:54:55 -0500

    Prémisses du premier unit test de ChargerLOB finie

--- JCAssertion/JCASQLODPCoreTest/LOBUT1.cs
+++ JCAssertion/JCASQLODPCoreTest/LOBUT1.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using JCASQLODPCore;
+using System.IO;
+ 
 
 
 namespace JCASQLODPCoreTest
@@ -10,6 +12,8 @@ namespace JCASQLODPCoreTest
     {
 
         JCASQLODPClient monSQLClient = new JCASQLODPClient();
+        String Chemin = JCAssertionCore.JCACore.RepertoireAssembly() +
+                 "Ressources\\";
 
 
         /// <summary>
@@ -36,7 +40,7 @@ namespace JCASQLODPCoreTest
                 "TYPECLOB,TYPEBLOB) " +
                 "VALUES('LOBUT1_3'," +
                 "'cas 3 des unit test lob','Info lob cas 3'," 
-                + "'Valeur VLOB à remplacer','F0F0')");
+                + "'Valeur CLOB à remplacer','F0F0')");
 
             // ,ettre un lob à remplacer
 
@@ -68,6 +72,56 @@ namespace JCASQLODPCoreTest
         [TestMethod]
         public void ChargeLOBOK()
         {
+            String FichierCLOB = Chemin + "CLOB.xml";
+            String FichierBLOB = Chemin + "BLOB.jpg";
+            // Test : Mattre à jour les champs BLOB (1 appel `ChargerLOB) 
+            // et CLOB (1 autre appel) du cas 1,
+            // ces colommes sont `null
+
+            // ensuite en un seul apppel mettre `jour les champs clob
+            // des cas 2 et 3
+            // Ensuite en un seul appel mettre
+            // à jour les colonnes  clob des cas 2 et 3
+            // 
+            // le cas 3 a des valeurs dams le clob et le blob
+            
+            // Vérifier les pré requis de l'essai
+            Assert.IsTrue(File.Exists(FichierCLOB),
+                "Le fichier CLOB d'essai est introubable " +
+                FichierCLOB);
+
+            Assert.IsTrue(File.Exists(FichierBLOB),
+                "Le fichier BLOB d'essai est introubable " +
+                FichierBLOB);
+
+            String monSQLCheck1 = "select count(*) from JCATest " +
+                "where IDTEST like 'LOBUT1%' and " +
+                " TYPECLOB is null";
+
+            Assert.IsTrue(
+            monSQLClient.AssertSQL(monSQLCheck1, 2),
+            "La base de données oracle ne contient pas les pré requis id=1");
+
+            String monSQLCheck2 = "select count(*) from JCATest " +
+                "where IDTEST like 'LOBUT1%' and " +
+                " TYPEBLOB is null";
+
+            Assert.IsTrue(
+            monSQLClient.AssertSQL(monSQLCheck2, 2),
+            "La base de données oracle ne contient pas les pré requis id=2");
+
+            String monSQLCheck3 = "select count(*) from JCATest " +
+                "where IDTEST like 'LOBUT1%' and " +
+                " TYPEBLOB is not null and " +
+                "TYPECLOB like '%Valeur CLOB à remplacer%'";
+
+            Assert.IsTrue(
+            monSQLClient.AssertSQL(monSQLCheck3, 1),
+            "La base de données oracle ne contient pas les pré requis id=3");
+ 
+ 
+
+
             Assert.Fail("Pas encore implémenté");
         }
     }

758b6b85220150f67082cfc584264eb6d8ae1ebb
Jean-Claude Parent <noursicain@gmail.com>
2017-05-26 22:11:29 -0400

    JCACompare.compare iunit tests passent

---
+++ JCAssertion/JCAssertionCoreTest/JCACompare.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JCAssertionCore;
+
+
+namespace JCAssertionCoreTest
+{
+    [TestClass]
+    public class JCACompareUT
+    {
+        [TestMethod]
+        public void JCACompare1()
+        {
+            JCACompare monCompare = new JCACompare();
+
+            Assert.IsTrue( 
+                monCompare.Compare(1,"=",1));
+
+            Assert.IsFalse(
+                monCompare.Compare(1, "=", 2));
+
+            Assert.IsTrue(
+                monCompare.Compare(1, "pg", 0));
+
+            Assert.IsTrue(
+                monCompare.Compare(1, "pg=", 1));
+
+
+            Assert.IsTrue(
+                monCompare.Compare(1, ">", 0));
+
+            Assert.IsTrue(
+                monCompare.Compare(1, ">=", 1));
+
+            // pp
+            Assert.IsTrue(
+                monCompare.Compare(1, "pp", 10));
+
+            Assert.IsTrue(
+                monCompare.Compare(1, "pp=", 1));
+
+
+            Assert.IsTrue(
+                monCompare.Compare(1, "<", 10));
+
+            Assert.IsTrue(
+                monCompare.Compare(1, "<=", 1));
+            // !=
+            Assert.IsTrue(
+                monCompare.Compare(12, "!=", 1));
+            Assert.IsTrue(
+                monCompare.Compare(12, "<>", 1));
+            // operateur invalide
+            try
+                {
+                    Assert.IsTrue(
+                        monCompare.Compare(12, "<xx>", 1));
+
+                    Assert.Fail("Une exception aurait du se produire");
+                } 
+            catch (Exception excep)
+            {
+                Assert.IsTrue(excep.Message.Contains(
+                    "Pour cette comparaison l'opérateur '<XX>' n'est pas un opérateur valide"),
+                    "Mauvais messages "+
+                    excep.Message  );     
+            }
+
+
+            
+        }
+    }
+}

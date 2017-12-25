4529dbfcd004cd5fd01fafeda36807157c0db081
Jean-Claude Parent <noursicain@gmail.com>
2017-12-25 11:42:09 -0500

    Créeation de la classe JCAXMLHelper+  unit tests

--- JCAssertion/JCAssertionCoreTest/ConnectionOracleUT1.cs
+++ JCAssertion/JCAssertionCoreTest/ConnectionOracleUT1.cs
@@ -54,5 +54,70 @@ namespace JCAssertionCoreTest
 
             
         }
+
+        /// <summary>
+        /// Test de l'assertion ConnectionOracle avec
+        /// action ce qui connecte 
+        /// immédiatement `la BD.
+        /// </summary>
+        [TestMethod]
+        public void OuvrirAvecActionConnectionOracle()
+        {
+            JCACore monCore = new JCACore();
+            JCAXMLHelper monxmlH = new JCAXMLHelper();
+            
+            // Définir connection
+            XmlDocument monCas = new XmlDocument();
+            monCas.InnerXml = monxmlH.xmlConnectionOracle (
+                "JCA",
+                "JCA",
+                "localhost",
+                "Ouvrir");
+            
+
+            monCore.ExecuteCas(monCas);
+
+            // assertions
+
+            Assert.IsTrue(monCore.Message.Contains(
+                "User : JCA"),
+                "Le message devrait contenir User : JCA");
+            Assert.IsTrue(monCore.Message.Contains(
+                "Password : JCA"),
+                "Le message devrait contenir Password : JCA");
+            Assert.IsTrue(monCore.Message.Contains(
+                "Serveur/instance : localhost"),
+                "Le message devrait contenir Serveur/instance : localhost");
+
+            Assert.IsTrue(monCore.Message.Contains(
+                "Assertion ConnectionOracle"),
+                "Le message devrait contenir Assertion ConnectionOracle");
+
+
+
+
+            Assert.IsTrue(
+                monCore.ODPSQLConnectionOuverte(),
+                "Le connection devrai être ouverte");
+            // Fermer la connection
+
+            monCas.InnerXml = monxmlH.xmlConnectionOracle(
+                "JCA2",
+                "JCA2",
+                "localhost2",
+                "Fermer");
+
+
+            monCore.ExecuteCas(monCas);
+
+            Assert.IsFalse(
+                monCore.ODPSQLConnectionOuverte(),
+                "La connection devrait êtrefermée");
+
+
+        }
+
+
+
     }
 }
---
+++ JCAssertion/JCAssertionnCore/JCAXMLHelper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+using System.Threading.Tasks;
+
+namespace JCAssertionCore
+{
+    public class JCAXMLHelper
+    {
+        public string xmlConnectionOracle(
+            String User,
+            String Password,
+            String Serveur,
+            String Action)
+        {
+            String Resultat = "";
+            Resultat = "<Assertion>" 
+                + Environment.NewLine +
+               "<Type>ConnectionOracle</Type>"
+               + Environment.NewLine;
+
+            if (User != null)
+                Resultat =
+                    Resultat
+                    + "<User>" + User
+                    + "</User>" + Environment.NewLine;
+
+            if (Password != null)
+                Resultat =
+                    Resultat
+                    + "<Password>" +
+                    Password + "</Password>" + Environment.NewLine;
+            if (Serveur != null)
+                Resultat =
+                    Resultat
+                    + "<Serveur>" + Serveur
+                    + "</Serveur>" + Environment.NewLine;
+
+            if (Action != null)
+                Resultat =
+                    Resultat
+                    + "<Action>" +
+                    Action + "</Action>" 
+                    + Environment.NewLine;
+
+            Resultat = Resultat +
+                "</Assertion>";
+
+            return Resultat;
+
+        }
+
+    }
+}
